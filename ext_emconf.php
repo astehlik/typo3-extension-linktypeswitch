d96e61cd9982274748d0fe827f5bc752a76c0877
Alexander Stehlik <alexander.stehlik@gmail.com>
2014-06-20 23:55:06 +0200

    [TASK] Adjusted requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'php' => '5.4.0-5.5.99',
-      'typo3' => '6.2.0-6.2.99',
+      'php' => '5.3.0-5.5.99',
+      'typo3' => '6.2.0-6.3.99',
     ),
     'conflicts' => 
     array (
